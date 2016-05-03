e110f8a4fb44e4a6a3f37b0196a3230e44aca6e1
Dzoka Mitrov <d.mitrov@gmail.com>
2016-05-03 15:06:12 +0200

    Initial Commit

---
+++ Close DEVs and RTCs.ps1
@@ -0,0 +1,16 @@
+﻿# This script kills all open NAV client processes
+
+$Process = Get-Process | Where-Object {$_.ProcessName -eq "fin" -or `
+                                       $_.ProcessName -eq "finsql" -or `
+                                       $_.ProcessName -like "*Nav.Client*"}
+
+if ($Process.Count -eq 0)
+    {exit}
+
+$Win32Processes =  Get-WmiObject win32_process | Select-Object ProcessId, Name,@{n='Owner';e={$_.GetOwner().User}} `
+                                               | Where-Object {$_.Owner -eq ([Environment]::UserName)} `
+                                               | Where-Object {$_.Name -like "finsql*exe" -or `
+                                                               $_.Name -like "fin*exe" -or ` 
+                                                               $_.Name -like "*Nav.Client*exe"} `
+                                               | ForEach-Object {$Process = Get-Process -Id $_.ProcessId 
+                                                                 $Process.CloseMainWindow() | Out-Null}
